177b79b8581eb05408fd3a56adfab0914e0a420a
0xPratik <sariapratik@gmail.com>
2025-09-17 22:14:48 +0545

    testing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Disable turbopack for production builds
 };
 
 export default nextConfig;
